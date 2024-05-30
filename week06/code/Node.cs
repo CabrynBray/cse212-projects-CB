cc31517087a75c7a5763cd5a0aa4ecc57c022b7e
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-05-30 16:17:36 +0200

    Trees Completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,36 +1,78 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value > Data)
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
+
+        else if (value == Data)
+        {
+            return;
+        }
     }
 
-    public bool Contains(int value) {
+    public bool Contains(int value)
+    {
         // TODO Start Problem 2
+        if (value < Data)
+        {
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+
+        }
+        if (value == Data)
+        {
+            return true;
+        }
+        if (value > Data)
+        {
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
         return false;
     }
 
-    public int GetHeight() {
+    public int GetHeight()
+    {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int heightLeft = 0;
+        int heightRight = 0;
+
+        if (Left is not null)
+        {
+            heightLeft = Left.GetHeight();
+        }
+        if (Right is not null)
+        {
+            heightRight = Right.GetHeight();
+        }
+        return Math.Max(heightLeft, heightRight) + 1;
     }
 }
